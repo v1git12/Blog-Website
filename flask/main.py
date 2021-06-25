5b33357a857f16e48744164d4fedba19d4350ae0
Vinayak Marathe <66788381+v1git12@users.noreply.github.com>
2021-06-25 08:35:23 +0530

    Add files via upload

--- flask/main.py
+++ flask/main.py
@@ -1,16 +1,23 @@
-# This is a sample Python script.
+from flask import Flask, render_template
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+@app.route('/post')
+def post():
+    return render_template('post.html')
+@app.route("/vinu")
+def vinu():
+    return "Vinu"
+app.run(debug=True)
